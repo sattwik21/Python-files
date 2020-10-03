bd9fc0301fc0bde5f60669d88f0016f2cd24f93f
Swastik Samal <51965359+Swastik-tsk@users.noreply.github.com>
2020-10-03 21:32:18 +0530

    Update Fibonacciseries.py

--- Fibonacciseries.py
+++ Fibonacciseries.py
@@ -1,5 +1,5 @@
 # Program to display the Fibonacci sequence up to n-th term
-nterms = int(input("How many terms? "))
+nterms = int(input("How many terms in the desired series ? "))
 n1, n2 = 0, 1
 count = 0
 if nterms <= 0:
